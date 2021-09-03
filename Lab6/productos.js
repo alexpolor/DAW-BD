b435093a917f1dd6ac6dc8f65ee54382accb0162
alexpolor <a01700769@itesm.mx>
2021-09-03 02:19:50 -0500

    lab6 completo

--- Lab6/productos.js
+++ Lab6/productos.js
@@ -1,10 +1,10 @@
-const formularioPedidos= document.getElementById("form");
-const pagar =document.getElementById("formPagar");
+const formularioPedidos = document.getElementById("form");
+const pagar = document.getElementById("formPagar");
 const pedidoMillion = document.getElementById('stockOneMillion');
 const pedidoBoss = document.getElementById('stockBossIntense');
 const pedidoInvictus = document.getElementById('stockInvictus');
-formularioPedidos.addEventListener("submit",pruebaStock);
-pagar.addEventListener("submit",pruebaBotonPagar);
+formularioPedidos.addEventListener("submit", pruebaStock);
+pagar.addEventListener("submit", pruebaBotonPagar);
 
 
 
@@ -21,50 +21,49 @@ var oneMillion = new Perfume(1, 'One Million', 'Paco Rabbane', 100, 1340, 5);
 var bossIntense = new Perfume(2, 'Bottled Intense', 'Hugo Boss', 100, 1299, 5);
 var invictus = new Perfume(3, 'Invictus', 'Paco Rabbane', 100, 1420, 5);
 
-document.getElementById("oneDescripcion").innerHTML = "Nombre: " + oneMillion.nombre + "<br>Marca: " + oneMillion.marca + "<br>Volumen: " + oneMillion.volumen + "ml<br>Precio: $" + oneMillion.precio.toLocaleString('en-US')+ "<br>Existencia: " +oneMillion.stock;
-document.getElementById("bossDescripcion").innerHTML = "Nombre: " + bossIntense.nombre + "<br>Marca: " + bossIntense.marca + "<br>Volumen: " + bossIntense.volumen +"ml<br>Precio: $" + bossIntense.precio.toLocaleString('en-US')+ "<br>Existencia: " +bossIntense.stock;
-document.getElementById("invictusDescripcion").innerHTML = "Nombre: " + invictus.nombre + "<br>Marca: " + invictus.marca + "<br>Volumen: " + invictus.volumen + "ml<br>Precio: $" + invictus.precio.toLocaleString('en-US')+ "<br>Existencia: " +invictus.stock ;
+document.getElementById("oneDescripcion").innerHTML = "Nombre: " + oneMillion.nombre + "<br>Marca: " + oneMillion.marca + "<br>Volumen: " + oneMillion.volumen + "ml<br>Precio: $" + oneMillion.precio.toLocaleString('en-US') + "<br>Existencia: " + oneMillion.stock;
+document.getElementById("bossDescripcion").innerHTML = "Nombre: " + bossIntense.nombre + "<br>Marca: " + bossIntense.marca + "<br>Volumen: " + bossIntense.volumen + "ml<br>Precio: $" + bossIntense.precio.toLocaleString('en-US') + "<br>Existencia: " + bossIntense.stock;
+document.getElementById("invictusDescripcion").innerHTML = "Nombre: " + invictus.nombre + "<br>Marca: " + invictus.marca + "<br>Volumen: " + invictus.volumen + "ml<br>Precio: $" + invictus.precio.toLocaleString('en-US') + "<br>Existencia: " + invictus.stock;
 
 //Siempre poner event
 function pruebaStock(event) {
     //Evita que refresque la pagina
     event.preventDefault();
-    if(existencias()==false){
+    if (existencias() == false) {
         alert("Verifique pedido, stock insufuciente");
-    }else{
-    calculoTotal(oneMillion.precio,bossIntense.precio,invictus.precio);
+    } else {
+        calculoTotal(oneMillion.precio, bossIntense.precio, invictus.precio);
     }
 }
 
-function existencias(){
-    if(pedidoMillion.value>oneMillion.stock || pedidoBoss.value>bossIntense.stock || pedidoInvictus.value>invictus.stock ){
+function existencias() {
+    if (pedidoMillion.value > oneMillion.stock || pedidoBoss.value > bossIntense.stock || pedidoInvictus.value > invictus.stock) {
         return false;
     }
 }
 
-function calculoTotal(a,b,c){
-    var iva=0;
-    var subTotal=0;
-    var total=0;
+function calculoTotal(a, b, c) {
+    var iva = 0;
+    var subTotal = 0;
+    var total = 0;
     var unidades;
-    let uniMillion=pedidoMillion.value;
-    let uniBoss=pedidoBoss.value;
-    let uniInvictus=pedidoInvictus.value;
+    let uniMillion = pedidoMillion.value;
+    let uniBoss = pedidoBoss.value;
+    let uniInvictus = pedidoInvictus.value;
 
-    unidades= uniMillion + uniBoss + uniInvictus;
-    subTotal=(a*pedidoMillion.value)+(b*pedidoBoss.value)+(c*pedidoInvictus.value);
-    iva=subTotal*0.16;
-    total=subTotal+iva;
+    unidades = uniMillion + uniBoss + uniInvictus;
+    subTotal = (a * pedidoMillion.value) + (b * pedidoBoss.value) + (c * pedidoInvictus.value);
+    iva = subTotal * 0.16;
+    total = subTotal + iva;
 
-    document.getElementById("descripcion").innerHTML="Subtotal------->$" +subTotal.toLocaleString('en-US')+ "<br>IVA---------------->$"+iva.toLocaleString('en-US')+ "<br>Total------------>$"+total.toLocaleString('en-US');
-    
-    
+    document.getElementById("descripcion").innerHTML = "Subtotal------->$" + subTotal.toLocaleString('en-US') + "<br>IVA----------------->$" + iva.toLocaleString('en-US') + "<br>Total------------->$" + total.toLocaleString('en-US');
+
+
 }
 
 
-function pruebaBotonPagar(event){
-    
+function pruebaBotonPagar(event) {
+
     alert("Gracias por tu compra");
-   
-}
 
+}
