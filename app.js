e860b5f4c53e0d45da30c0f8b001779f7a1594dd
alexpolor <a01700769@itesm.mx>
2021-09-20 17:11:27 -0500

    Configurar Body Parser para obtener datos de usuaro

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/alguna-ruta', (request, response, next) => {
+    console.log(request.body);
+});
+
 app.use((request,response, next) => {
     console.log("Primer Middleware");
     next ();
@@ -12,6 +18,14 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+app.use('/ruta/nombres', (request, response, next) => {
+    response.send('Respuesta de la ruta "/ruta/nombres"'); 
+});
+
+app.use('/ruta/apellidos', (request, response, next) => {
+    response.send('Respuesta de la ruta "/ruta/apellidos"'); 
+});
+
 app.use('/ruta', (request, response, next) => {
     response.send('Respuesta de la ruta "/ruta"'); 
 });
@@ -20,7 +34,6 @@ app.use((request, response, next) => {
     console.log('Otro middleware!');
     response.send('¡Hola mundo!'); //Manda la respuesta
 });
-
 
 
 app.listen(3000);
